c1a799e484b8c6e80f6c4380e059fa4dd7a4f731
SomSom-Pimnara <computermatthayom3.1som@gmail.com>
2025-08-30 17:14:35 +0700

    Promotion

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,8 @@ import AdminReport from "./pages/admin/admin_report";
 import AdminRider from "./pages/admin/admin_rider";
 import AdminRestaurants from "./pages/admin/admin_restaurant";
 import AdminProfile from "./pages/admin/admin_profile";
+import AdminPromotion from "./pages/admin/admin_promotion";
+
 
 // User pages
 import RestaurantDetailPage from "./pages/RestaurantDetailPage";
@@ -94,6 +96,7 @@ export default function App() {
         <Route path="rider" element={<AdminRider />} />
         <Route path="restaurant" element={<AdminRestaurants />} />
         <Route path="profile" element={<AdminProfile />} />
+        <Route path="promotion" element={<AdminPromotion />} />
       </Route>
     </Routes>
   );
